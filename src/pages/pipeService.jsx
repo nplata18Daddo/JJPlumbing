312ff3105b82d3bd3d3054906af91d62b55e6bd3
nplata18Daddo <na.plata@daddosoftware.onmicrosoft.com>
2022-10-31 22:15:16 -0500

    Avance detail services

---
+++ src/pages/pipeService.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PipeService = () => {
+  return <div>pipeService</div>;
+};
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,23 @@
 import { Route, Routes } from "react-router";
 import { Layout } from "../components/layout/layout";
+import { CameraService } from "../pages/cameraService";
+import { DrainCleaningService } from "../pages/drainCleaningService";
 import { Home } from "../pages/home";
+import { PipeService } from "../pages/pipeService";
+import { PlumbingService } from "../pages/plumbingService";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/services/plumbing" element={<PlumbingService />} />
+        <Route
+          path="/services/draingCleaning"
+          element={<DrainCleaningService />}
+        />
+        <Route path="/services/pipeDescaling" element={<PipeService />} />
+        <Route path="/services/cameraInspection" element={<CameraService />} />
       </Route>
     </Routes>
   );
