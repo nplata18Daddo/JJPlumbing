753b430d0fc676525edd89d8b70359b69542856c
nplata18Daddo <na.plata@daddosoftware.onmicrosoft.com>
2022-12-13 09:35:20 -0500

    Cambios imagenes

--- src/pages/plumbingService.jsx
+++ src/pages/plumbingService.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import Carousel from "react-bootstrap/Carousel";
 export const PlumbingService = () => {
   return (
     <div className="services__container" style={{ paddingTop: "2rem" }}>
@@ -49,13 +50,36 @@ export const PlumbingService = () => {
             className="my-auto"
             style={{ textAlign: "center", justifyContent: "center" }}
           >
-            <img
-              style={{
-                width: "60%",
-                borderRadius: "30px",
-              }}
-              src={`/images/plumbingPicture.png`}
-            ></img>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="/images/plumbing1.jpeg"
+                  alt="Third slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="/images/plumbing2.jpeg"
+                  alt="Third slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="/images/plumbing3.jpeg"
+                  alt="Third slide"
+                />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="/images/plumbing4.jpeg"
+                  alt="Fourth slide"
+                />
+              </Carousel.Item>
+            </Carousel>
           </Col>
         </Row>
 
